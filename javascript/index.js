aef3ab11f29c6069a81651c29022a1978de2d80c
ls <izqiaulhassan166@gmail.com>
2026-07-10 22:51:28 +0500

    js added

---
+++ javascript/index.js
@@ -0,0 +1,28 @@
+// var in js
+var name = "ali";
+console.log(name)
+var name = "ali"
+console.log(name)
+name = "ahmed"
+console.log(name)
+
+// var can be redeclared and reassigned and is function scoped.
+
+
+// let in js 
+let member = "ahmed"
+console.log(member)
+member = "hassan"
+console.log(member)
+let member = "khan"
+console.log(member)
+
+// let can be reassigened but cannot be redeclared and is block scoped 
+
+// const in js 
+const number  = 22 ;
+console.log(number)
+number = 34
+console.log(number)
+
+// const cannot be reassigned and cannot be redeclared and is  block scoped 
